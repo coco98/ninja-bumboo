c59c47a14a72a389b44a25071bc6409d6ccce4b9
Tanmai Gopal <tanmaig@34cross.in>
2019-01-27 12:49:00 +0530

    Updates button styling

--- src/auth0-variables.js
+++ src/auth0-variables.js
@@ -1,5 +1,7 @@
 export const AUTH_CONFIG = {
   domain: 'ninja-bumboo.auth0.com',
   clientId: '1FdfUnOzUxBLMTVJEnGQeuiHB3l61NTc',
+  // FIXME: Change to env vars here
   callbackUrl: 'https://ninja-bumboo.netlify.com/callback'
+  // callbackUrl: 'http://localhost:3000/callback'
 };
